f869bfa7e1c30b1b132ad2f37057bc84c9d47186
Allan Zeta <aczeta180@gmail.com>
2023-11-29 21:37:49 +0800

    initial commit

---
+++ home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "aczet";
+  home.homeDirectory = "/home/aczet";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  # collection of programs without generated configuration
+  home.packages = [
+    pkgs.gh   
+    pkgs.ripgrep
+    pkgs.jq
+    pkgs.yq-go
+    pkgs.fzf
+  ];
+  # Install git
+  programs.git = {
+    enable = true;
+    userName = "Allan Zeta";
+    userEmail = "aczeta180@gmail.com";
+    lfs = {
+      enable = true;
+      };
+    };
+  # Install and configure bash
+  programs.bash = {
+    enable = true;
+    sessionVariables = {
+      EDITOR = "vim";
+      };
+    }; 
+    
+}
