4524daf2e4377020262b4cc42085ed9d41ac5286
Allan Zeta <aczeta180@gmail.com>
2023-12-01 18:59:15 +0800

    initial setup for neovim

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 {
   # import packages
   imports = [
-    ./programs/nvim
+      ./programs/nvim
   ];
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
--- programs/nvim/default.nix
+++ programs/nvim/default.nix
@@ -1,44 +1,62 @@
-{ pkgs, ... }: {
+{pkgs, ...}:
+
+{
   programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    defaultEditor = true;
-    extraLuaConfig = ''
-      vim.cmd("syntax on")
-      vim.cmd("set hlsearch")
-      vim.cmd("set ignorecase")
-      vim.cmd("set incsearch")
-      vim.cmd("set number")
-      vim.cmd("set noswapfile")
-      vim.cmd("let mapleader = \"'\" ")
-      vim.cmd("inoremap jk <ESC>")
-      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-      if not vim.loop.fs_stat(lazypath) then
-      vim.fn.system({
-         "git",
-         "clone",
-         "--filter=blob:none",
-         "https://github.com/folke/lazy.nvim.git",
-         "--branch=stable", -- latest stable release
-         lazypath,
-         })
-     end
-     vim.opt.rtp:prepend(lazypath)
-     require("lazy").setup({
-         "toppair/peek.nvim",
-         event = { "VeryLazy" },
-         build = "deno task --quiet build:fast",
-         config = function()
-           require("peek").setup({
-	     app = 'chromium'})
-           -- refer to `configuration to change defaults`
-           vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-           vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-      end,
-      }
-      
-      )
-    '';
-  }; 
-}
+  enable = true;
+  viAlias = true;
+  vimAlias = true;
+  defaultEditor = true;
+  extraLuaConfig = ''
+    vim.cmd("syntax on")
+    vim.cmd("set hlsearch")
+    vim.cmd("set ignorecase")
+    vim.cmd("set incsearch")
+    vim.cmd("set number")
+    vim.cmd("set noswapfile")
+    vim.cmd("set nocompatible")
+    vim.cmd("set showmatch")
+    vim.cmd("set tabstop=4")
+    vim.cmd("set expandtab")
+    vim.cmd("set shiftwidth=4")
+    vim.cmd("set wildmode=longest,list")
+    vim.cmd("set cc=80")
+    vim.cmd("set cursorline")
+    vim.cmd("set clipboard=unnamedplus")
+    vim.cmd("set ttyfast")
+    vim.cmd("let mapleader = \",\" ")
+    vim.cmd("inoremap jk <ESC> " )
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
+    require("lazy").setup(
+    {
+        {
+            "folke/which-key.nvim",
+            event = "VeryLazy",
+            init = function()
+                vim.o.timeout = true
+                vim.o.timeoutlen = 300
+            end
+        },
+        {
+            "folke/tokyonight.nvim",
+            lazy = false,
+            priority = 1000
+        }
+    })
+    vim.cmd("colorscheme tokyonight")
+  '';
+  
+  };
+
+
+} 
